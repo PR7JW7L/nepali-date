6118691b7da25d67a1760663d656b3652e42b81e
prajwal <prajwal@yarsa.io>
2025-12-08 15:20:05 +0545

    added umd build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from "tsup";
-export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
-  dts: true,
-  minify: true,
-  sourcemap: true,
-  clean: true,
-  target: "es2020",
-  treeshake: true,
-});
+
+export default defineConfig([
+  {
+    entry: ["src/index.ts"],
+    format: ["esm", "cjs"],
+    dts: true,
+    minify: true,
+    sourcemap: true,
+    clean: true,
+    target: "es2020",
+    treeshake: true,
+  },
+  {
+    entry: ["src/index.ts"],
+    format: ["iife"],
+    globalName: "NepaliDateLib",
+    minify: true,
+    sourcemap: true,
+    target: "es2020",
+    outExtension: () => ({ js: ".global.js" }),
+  },
+]);
